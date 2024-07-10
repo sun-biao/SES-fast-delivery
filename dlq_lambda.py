5805e0f0ec5590259dd1dc2f9257c1ef8a5a1cce
sunbiao <43977775+sun-biao@users.noreply.github.com>
2024-07-10 14:31:13 +0800

    Update dlq_lambda.py

--- dlq_lambda.py
+++ dlq_lambda.py
@@ -6,9 +6,9 @@ import boto3
 
 
 db = mysql.connector.connect(
-    host="email.cluster-cp2k0i0iuocy.us-west-2.rds.amazonaws.com",
-    user="admin",
-    password="admin1234",
+    host="<HOST_RDS>",
+    user="<USERNAME>",
+    password="<PWD>",
     database="email"
 )
 cursor = db.cursor()
